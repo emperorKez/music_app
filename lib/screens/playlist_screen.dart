be16b04e27a2a0ed6bf9c284e9731371876fba42
Johnson Nwachukwu <johnsonkesie@gmail.com>
2023-09-03 21:53:37 +0100

    code update

--- lib/screens/playlist_screen.dart
+++ lib/screens/playlist_screen.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:music_app/data.dart';
-import 'package:music_app/pages/now_playing.dart';
+
 
 class PlaylistScreen extends StatelessWidget {
   const PlaylistScreen(
